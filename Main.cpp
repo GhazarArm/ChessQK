40ccb2387b56ff3ec727b2f7b0ad9431ea0e0c27
GhazarArm <terxachatryan20@gmail.com>
2016-12-30 01:42:01 +0400

    Ghazar commit 7

--- Main.cpp
+++ Main.cpp
@@ -1,28 +1,24 @@
 ﻿#include "Queen.h"
-
+#include "Knight.h"
+using namespace std;
 int main () 
 {
 	Figure* f;
+
 	int your_wanted_problem;
 	cin>>your_wanted_problem;
-	
+
 	if(your_wanted_problem == 1)
 	{
-		Position start_pos(0 , 0 , black);
+		Position start_pos(4 , 5 , black);
 		f = new Queen(start_pos);
-	}
-	else 
-	if(your_wanted_problem == 0)
-	{
-		f = new Knight(2 , 2);
 	}
 	else
 	{
-		cout<<"error";
+		f = new Knight(4 , 5);
 	}
 
-	f->problem();
-	f->show();
-
+	f->Problem();
+	f->Show();
 	return 0;
 }
